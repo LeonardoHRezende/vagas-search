07fc00bae6e32c1dfcfc2ca93365b9ff23065276
LeonardoHRezende <rezendeleo64@gmail.com>
2023-02-02 21:51:21 -0300

    Improvement: melhoria de retorno

--- server/routes/searchUser.ts
+++ server/routes/searchUser.ts
@@ -5,23 +5,27 @@ const app = Express();
 
 app.get('/', async (req, res) => {
 
+    //busca usuário
     const searchUsers = await prisma.user.findMany();
 
     const users = await Promise.all(
         searchUsers.map(async (user) => {
 
+            //adiciona habilidades do usuario ao obj de retorno
             const habilits = await prisma.habilits.findMany({
                 where: {
                     userId: user.id
                 }
             });
 
+            //adiciona formação academica do usuario ao obj de retorno
             const academicFormation = await prisma.academicFormation.findMany({
                 where: {
                     userId: user.id
                 }
             });
 
+            //adiciona experiencias do usuario ao obj de retorno
             const experience = await prisma.experience.findMany({
                 where: {
                     userId: user.id
@@ -32,7 +36,7 @@ app.get('/', async (req, res) => {
         })
     );
 
-    res.send(users);
+    res.status(200).json(users);
 })
 
 export default app;
