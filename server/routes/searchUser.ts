1fd94b3da01e1efd2c888639385445d2dd2b77c7
LeonardoHRezende <rezendeleo64@gmail.com>
2023-02-02 21:49:47 -0300

    Feat: consulta de todos os dados relacionados ao usuário

--- server/routes/searchUser.ts
+++ server/routes/searchUser.ts
@@ -5,9 +5,34 @@ const app = Express();
 
 app.get('/', async (req, res) => {
 
-    const searchUser = await prisma.habilits.findMany();
+    const searchUsers = await prisma.user.findMany();
 
-    res.send(searchUser);
+    const users = await Promise.all(
+        searchUsers.map(async (user) => {
+
+            const habilits = await prisma.habilits.findMany({
+                where: {
+                    userId: user.id
+                }
+            });
+
+            const academicFormation = await prisma.academicFormation.findMany({
+                where: {
+                    userId: user.id
+                }
+            });
+
+            const experience = await prisma.experience.findMany({
+                where: {
+                    userId: user.id
+                }
+            });
+
+            return { user, habilits, academicFormation, experience };
+        })
+    );
+
+    res.send(users);
 })
 
 export default app;
